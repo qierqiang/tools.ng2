b6ba52c7e96a3083510f99ea703e26ff5ac20566
qierqiang <erqiang.qi@gmail.com>
2016-10-22 20:42:25 +0800

    添加main.js

---
+++ app/main.js
@@ -0,0 +1,6 @@
+define(["require", "exports", '@angular/platform-browser-dynamic', './app.module'], function (require, exports, platform_browser_dynamic_1, app_module_1) {
+    "use strict";
+    const platform = platform_browser_dynamic_1.platformBrowserDynamic();
+    platform.bootstrapModule(app_module_1.AppModule);
+});
+//# sourceMappingURL=main.js.map
